4e870b97769c65fe83cf7e19a66ff77fe79a1433
teena ajith <teena.ajith@gmail.com>
2020-03-04 11:42:40 +0100

    Established connection between dog and schedule

--- db/migrate/20200303171842_remove_schedule_from_dogs.rb
+++ db/migrate/20200303171842_remove_schedule_from_dogs.rb
@@ -1,5 +1,5 @@
 class RemoveScheduleFromDogs < ActiveRecord::Migration[5.2]
-  def change
-    remove_foreign_key :dogs, column: :schedule_id
+  def self.down
+      remove_column :dogs, :schedule_id
   end
 end
