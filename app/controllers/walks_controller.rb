980fc4a924d34fd223e51155596136242f0e76a7
teena ajith <teena.ajith@gmail.com>
2020-03-09 09:36:24 +0100

    made schedule link working

--- app/controllers/walks_controller.rb
+++ app/controllers/walks_controller.rb
@@ -2,18 +2,19 @@ class WalksController < ApplicationController
 
   def schedule
     @walk = Walk.find_by date: params[:date]
-    # this is test
+
     @slot = Slot.new(walk: @walk)
 
+    today = params[:date].to_date
 
-    today = Date.today
+    # if i check on saturday/sunday it shows last week's. so I chaged to show next week schedule
+    if today.saturday? || today.sunday?
+      today = today.next_week
+    end
+
     days_from_this_week = today.at_beginning_of_week..today.at_end_of_week
     @days_from_this_week = days_from_this_week.reject{ |date| date.saturday? || date.sunday? }
   end
-
-
-
-
 
 end
 
@@ -21,3 +22,4 @@ end
 # <%= link_to (Date.today).strftime("%A"), walks_schedule_path(Date.today) %>
 
 # <%= link_to (Date.today+1).strftime("%A"), walks_schedule_path(Date.today+1) %>
+# without
